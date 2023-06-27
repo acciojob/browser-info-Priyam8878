9cd2318ff5520c5913ff176dc6c4641575ca0bf2
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-27 17:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const para = document.createElement("div");
+para.innerHTML = "You are using "+browserName + "  " + versionName
+document.getElementById("browser-info").appendChild(para);
