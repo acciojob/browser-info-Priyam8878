393027edada3cfb114d7b03e69478c672db4c48b
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-27 17:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 const browserName = navigator.appCodeName;
-const versionName = navigator. appVersion
+const versionName = navigator. appVersion;
+
 const para = document.getElementById("browser-info");
-para.innerHTML = "You are using "+browserName + "  " + versionName
+para.innerHTML = "You are using "+browserName + " version " + versionName
